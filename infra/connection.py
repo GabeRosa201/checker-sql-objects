7c7c44f792f05d705103766b7be53337e3815b25
gabrosa <gabriel.melo@bandtec.com.br>
2025-08-23 10:49:05 -0300

    add - classe de conexão com o banco de dados

---
+++ infra/connection.py
@@ -0,0 +1,13 @@
+class Connection():
+
+    def __init__(self,):
+        pass
+
+    def connect_database(self, connection_string:str):
+        pass
+
+    def desconect(self):
+        pass
+
+    def execute_query(self, query:str, params:list):
+        pass
