06a6de4fe94719986bc0e3ad6797caeba5ab6625
gabrosa <gabriel.melo@bandtec.com.br>
2025-08-25 21:15:01 -0300

    add - inicio do comparador dos processos

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 import time
+from infra.connection import Connection
 
 if __name__ == '__main__':
     print('A verrudara dos arquivos irá começar em breve')
 
     time.sleep(3)
+
+    # Operação
+
+    # Informar a base modelo
+    base_modelo = {
+        'nome': '',
+        'server': '',
+        'user': '',
+        'secret': '',
+        'is_windows_auth': False
+    }
+
+    base_a_ser_comparada = {
+        'nome': '',
+        'server': '',
+        'user': '',
+        'secret': '',
+        'is_windows_auth': False
+    }
+
+    conn = Connection()
+    conn.connect_database(
+        base_modelo['nome'],
+        base_modelo['server'],
+        '',
+        '',
+        base_modelo['is_windows_auth']
+    )
 
     print('Obrigado por utilizar nosso sistema!')
