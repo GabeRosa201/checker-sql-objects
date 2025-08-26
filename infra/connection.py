d53b579de44f86e6932a1766ddad2147fcdf9dba
gabrosa <gabriel.melo@bandtec.com.br>
2025-08-25 21:10:41 -0300

    add - metodos de conectar e desconectar

--- infra/connection.py
+++ infra/connection.py
@@ -2,14 +2,50 @@ import pyodbc
 
 class Connection():
 
-    def __init__(self,):
+    def __init__(self):
+        self._connection = None
+        self._driver = 'ODBC Driver 17 for SQL Server'
         pass
 
-    def connect_database(self, connection_string:str):
-        pass
+    def connect_database(self, 
+                         database:str, 
+                         server:str, 
+                         user: str,
+                         pwd: str,
+                         is_windows_auth: str):
+        
+        """
+        Estabelece a conexão com o SQL Server.
+        """
+        try:
+            if user and pwd:
+                self._connection = pyodbc.connect(
+                    f"DRIVER={{{self._driver}}};"
+                    f"SERVER={server};"
+                    f"DATABASE={database};"
+                    f"UID={user};"
+                    f"PWD={is_windows_auth};"
+                )
+            else:
+                # Autenticação integrada do Windows
+                self._connection = pyodbc.connect(
+                    f"DRIVER={{{self._driver}}};"
+                    f"SERVER={server};"
+                    f"DATABASE={database};"
+                    f"Trusted_Connection=yes;"
+                )
+
+            print("✅ Conexão estabelecida com sucesso!")
+            return self._connection
+        
+        except Exception as e:
+            print(f"❌ Erro ao conectar no SQL Server: {e}")
+            return None
 
     def desconect(self):
-        pass
+        if self._connection:
+            self._connection.close()
+            print("🔒 Conexão fechada.")
 
     def execute_query(self, query:str, params:list):
         pass
